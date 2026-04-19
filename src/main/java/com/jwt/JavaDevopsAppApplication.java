0e2b0d4cb904637282a0c8194c1769221efc617b
itshrey <itshrey2509@gmail.com>
2026-04-19 16:38:06 +0530

    Initial commit - Java DevOps project

---
+++ src/main/java/com/jwt/JavaDevopsAppApplication.java
@@ -0,0 +1,24 @@
+package com.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+public class JavaDevopsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevopsAppApplication.class, args);
+	}
+
+}
+
+
+@RestController
+class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Java DevOps App Running 🚀";
+    }
+}
